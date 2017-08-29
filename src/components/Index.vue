2a683d75b419cfc6a1bbf63029b4d56d43b4382b
souky <136069803@qq.com>
2017-08-29 10:33:55 +0800

    bug fix

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="index" class="main_body">
-	{{msg}}
+	
 </div>
 </template>
 
@@ -12,12 +12,7 @@ export default {
     }
   },
   mounted:function(){
-  	var dates = new Date();
-  	var dateString = this.timeF(dates).format("YYYY-MM-DD HH:mm:ss");
-  	this.msg = dateString;
-  	this.postHttp(this,{},"auth/user/getLoginUser",function(obj,data){
-  		
-  	})
+  	
   }
 }
 </script>
